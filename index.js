f3f66eb75707e206370c7fdee08eac2fbc2a7cb7
Maaniksharma <manikss123456@gmail.com>
2025-03-12 11:43:17 +0530

    INIT

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-//sending data from client using form
 const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.static("public"));
 
-app.post("/form-submit", (req, res) => {
-  console.log(req.body);
-  res.send("data recieved");
+const USERS = [];
+
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  USERS.push({
+    username: username,
+    password: password,
+  });
+  console.log(USERS);
+  res.redirect("/login.html");
 });
 
-app.listen(port, () => console.log(`app listening on port ${port}!`));
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const filteredUsers = USERS.filter((user) => user.username === username);
+  if (filteredUsers.length === 0) {
+    res.send("invalid username");
+    return;
+  }
+  if (filteredUsers[0].password === password) {
+    res.redirect("/Dashboard.html");
+  } else {
+    res.send("Invalid password");
+  }
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
